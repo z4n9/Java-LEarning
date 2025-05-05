ea102434b5e603c783e86327bf30786290af20c9
z4n9 <arthur1820@z4n9pc.com>
2025-05-05 23:28:31 +0500

    Interface Segregation

--- src/SOLID/Interface_Segregation.java
+++ src/SOLID/Interface_Segregation.java
@@ -2,6 +2,42 @@ package SOLID;
 
 public class Interface_Segregation {
     public static void main(String[] args) {
-        
+        Duck duck = new Duck();
+		duck.fly();
+		duck.swim();
+
+		Fish fish = new Fish();
+		fish.swim();
     }
+}
+
+interface Flyable{
+	void fly();
+}
+
+interface Swimable{
+	void swim();
+}
+
+interface Animal extends Flyable, Swimable{
+
+}
+
+class Duck implements Animal{
+	@Override 
+	public void fly(){
+		System.out.println(getClass() + " Flying");		
+	}
+
+	@Override 
+	public void swim(){
+		System.out.println(getClass() + " Swimming");
+	}
+}
+
+class Fish implements Swimable{
+	@Override
+	public void swim(){
+		System.out.println(getClass() + " Swimming");		
+	}
 }
