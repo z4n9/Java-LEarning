a6b12bd7e923092ae854038ca312eee377b04866
z4n9 <arthur1820@z4n9pc.com>
2025-05-15 16:43:42 +0500

    Dependency inversion

---
+++ src/SOLID/Dependency_Inversion.java
@@ -0,0 +1,41 @@
+package SOLID;
+
+public class Dependency_Inversion {
+    public static void main(String[] args) {
+
+        SportClub sportClub = new SportClub();
+        sportClub.consist();
+
+    }
+}
+
+interface Sportsmen{
+    void run();
+}
+
+class Basketballer implements Sportsmen{
+    @Override
+    public void run() {
+        System.out.println(getClass() + " is running");
+    }
+}
+
+class Footballer implements Sportsmen{
+    @Override
+     public void run(){
+        System.out.println(getClass() + " is running");
+    }
+}
+
+class SportClub{
+    Sportsmen sportsmen1;
+    Sportsmen sportsmen2;
+
+    void consist(){
+        sportsmen1 = new Footballer();
+        sportsmen1.run();
+
+        sportsmen2 = new Basketballer();
+        sportsmen2.run();
+    }
+}
